c5bc32e34e5f431dcf9b9c087cd4d825c9d8ffa9
Justin Priest <justin.priest@alaska.gov>
2021-05-22 18:14:53 -0800

    created age-length density figure saved density figure in output

--- code/1_demoscript_cohoages.R
+++ code/1_demoscript_cohoages.R
@@ -6,7 +6,6 @@
 
 
 
-
 #####      DEMO SCRIPT      ####
 # Data are from ADF&G Coho Collections
 # Shaul et al. 2019
@@ -15,13 +14,12 @@
 # http://www.adfg.alaska.gov/FedAidPDFs/RIR.1J.2019.12.pdf
 
 
+### LIBRARIES
 library(tidyverse)
 library(lubridate)
 library(ggridges)
 
-
-
-
+### DATA IMPORT
 agedata <- read_csv("data/ADFG_smoltages_AukeBernersHughSmith.csv") %>%
   mutate(Date = ymd(as.POSIXct(Date, format = "%m/%d/%Y", tz = "US/Alaska")),
          species = "Coho Salmon",
@@ -36,7 +34,6 @@ agedata <- read_csv("data/ADFG_smoltages_AukeBernersHughSmith.csv") %>%
   dplyr::select(species, location, year, everything()) # reorder columns
 
 agedata # View data 
-
 
 
 ### FIGURES
@@ -55,4 +52,32 @@ ggplot(agedata, aes(x = as.factor(fw_age), y = length_mm, fill = as.factor(fw_ag
     strip.background = element_rect(fill = NA, color = NA),
     strip.text.x = element_text(color = "gray30"),
   )
+
+
+
+densityplot <- ggplot(agedata, aes(x = length_mm, fill = as.factor(fw_age))) + 
+  geom_density(alpha = 0.5) +
+  scale_fill_manual(values = c("#F78D71", "#B5517D", "#463075")) +
+  labs(x = "Length (mm)",
+       y = "Density proportion") +
+  facet_wrap(~location) +
+  theme_light() +
+  theme(
+    legend.position = "none",
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    strip.background = element_rect(fill = NA, color = NA),
+    strip.text.x = element_text(color = "gray30"),
+  )
+densityplot
+ggsave("output/densityfig.png", densityplot, width = 6, height = 3, units = "in", dpi = 300)
+
+
+
+# Exploratory geom_ridges plot
+
+ggplot(agedata, aes(y = year, x= length_mm, group = year)) +
+  geom_density_ridges(alpha = 0.8, fill = "#B5517D") +
+  theme_light() +
+  facet_wrap(~location)
 
